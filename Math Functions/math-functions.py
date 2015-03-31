e0275f8beb14d6ba590e5ae59c50e6911761b94d
georgeleedotco <g@georgelee.co>
2015-03-30 23:34:10 -0400

    updates

--- Math Functions/math-functions.py
+++ Math Functions/math-functions.py
@@ -1,38 +1,63 @@
-# File:    mathfunctions.py
+# File:    math-functions.py
 # Author:  George Lee
 # Date:    3/30/2015
 # E-mail:  g@georgelee.co
 # Web: georgelee.co
-# © Copyright 2014 George Lee / georgelee.co. All Rights Reserved
+# © Copyright 2015 George Lee / georgelee.co. All Rights Reserved
 # Description:
 # This program contains simple math functions that get the maximum
 # minimum, average/mean
 
 def getMax(myList):
-	maxVal = 0
-	for e in myList:
-		if(e > maxVal):
-			maxVal = e
-	return maxVal
+    maxVal = 0
+        for e in myList:
+            if(e > maxVal):
+                maxVal = e
+        return maxVal
 
 def getMin(myList):
-	minVal = myList[0]
-	for e in myList:
-		if(e < minVal):
-			minVal = e
-	return minVal
+    minVal = myList[0]
+        for e in myList:
+            if(e < minVal):
+                minVal = e
+        return minVal
 
-def getMean(myList): 
+def getMean(myList):
     total = 0
     average = 0
     for e in myList:
         total += e
-        average = total/len(myList) 
+        average = total/len(myList)
     return average
 
+def getMedian(myList):
+    # bubble sort!
+    isSorted = False
+        
+        while(isSorted != True):
+            isSorted = True
+                
+                for b in range(0, len(myList) - 1):
+                    
+                    if(myList[b] > myList[b+1]):
+                        
+                        isSorted = False
+                            
+                            larger = myList[b]
+                                smaller = myList[b+1]
+                                myList[b+1] = larger
+                                myList[b] = smaller
+        
+        sortedList = myList
+        
+    return (myList[len(sortedList) // 2])
+
+
+
 def main():
-	print(getMax([2,4,7]))
-	print(getMin([2,4,7]))
-	print(getMean([2,4,7]))
+    print(getMax([2,4,7]))
+        print(getMin([2,4,7]))
+        print(getMean([2,4,7]))
+        print(getMedian([10, 11, 13, 15, 16, 23, 26]))
 
 main()
